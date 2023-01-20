bcebbffe53d8db16306ae933bf31da0c7b123cb5
thucnguyen99 <68426662+thucnguyen99@users.noreply.github.com>
2023-01-21 00:40:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -29,4 +29,40 @@ var design = anime({
   direction: 'alternate',
   loop: true
 });
+
+// To disable right click
 document.addEventListener('contextmenu', event => event.preventDefault());
+
+// To disable F12 options
+document.onkeypress = function (event) {
+event = (event || window.event);
+if (event.keyCode == 123) {
+return false;
+}
+}
+document.onmousedown = function (event) {
+event = (event || window.event);
+if (event.keyCode == 123) {
+return false;
+}
+}
+document.onkeydown = function (event) {
+event = (event || window.event);
+if (event.keyCode == 123) {
+return false;
+}
+}
+
+// To To Disable ctrl+c, ctrl+u
+
+jQuery(document).ready(function($){
+$(document).keydown(function(event) {
+var pressedKey = String.fromCharCode(event.keyCode).toLowerCase();
+
+if (event.ctrlKey && (pressedKey == "a" || pressedKey == "u")) {
+alert('Sorry, This Functionality Has Been Disabled!');
+//disable key press porcessing
+return false;
+}
+});
+});
